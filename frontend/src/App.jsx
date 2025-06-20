621ec28fed07ad90190be07cd9b6dda3c59994bc
Saurav Kumar <magnus@Sauravs-MacBook-Pro.local>
2025-06-20 20:33:38 +0530

    Adding Footer with initial Styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import MyEvents from './pages/MyEvents'
 import Events from './pages/Events'
+import Footer from './components/Footer.jsx'
 const App = () => {
   return (
+    <>
+    <Footer/>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<MyEvents />} />
         <Route path="/events" element={<Events />} />
       </Routes>
+      <Footer/>
     </Router>
+    </>
   );
 };
 

d5f9659dfe56a39275404c5978c7ecf91f87fef0
Saurav Kumar <magnus@Sauravs-MacBook-Pro.local>
2025-06-26 15:08:47 +0530

    adding initial backend

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,13 @@
 // server.js
-import http from 'http'
+import express from 'express';
 
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({
-    status: 'success',
-    message: 'Pure Node backend is running 🚀'
-  }))
-})
+app.get('/', (req, res) => {
+  res.send('Campus Buddy API is running');
+});
 
-server.listen(PORT, () => {
-  console.log(`🌐 Server running at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});

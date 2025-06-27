2aa634ebeda6cf824e5e7acac365f469e83b1e72
TechGenie-awake <techgenie2050@gmail.com>
2025-06-27 14:02:23 +0530

    Event Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
 import MyEvents from "./pages/MyEvents";
-import Events from './pages/Events'
-import Footer from './components/Footer.jsx'
-import AuthPage from './pages/AuthPage';
-import ProfileCard from './pages/ProfileCard';
-import ClubProfileCard from './pages/ClubProfileCard'
-import CreateEventForm from './pages/CreateEventForm';
-import ClubDirectory from './pages/ClubDirectory';
+import Events from "./pages/Events";
+import Footer from "./components/Footer.jsx";
+import AuthPage from "./pages/AuthPage";
+import ProfileCard from "./pages/ProfileCard";
+import ClubProfileCard from "./pages/ClubProfileCard";
+import CreateEventForm from "./pages/CreateEventForm";
+import ClubDirectory from "./pages/ClubDirectory";
+import CampusEvents from "./pages/CampusEvents.jsx";
 
 const App = () => {
   return (
     <>
-    <Router>
-      <div>
+      <Router>
         <div>
-          <Routes>
-            <Route path="/" element={<Navigate to="/home" replace />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/myevents" element={<MyEvents />} />
-            <Route path="/events" element={<Events />} />
-            <Route path='/profilecard' element={<ProfileCard/>} />
-            <Route path='/clubprofilecard' element={<ClubProfileCard/>} />
-            <Route path="/login" element={<AuthPage />} />
-            <Route path="/createevent" element={<CreateEventForm />} />
-            <Route path='/clubpage' element={<ClubDirectory/>}/>
-          </Routes>
+          <div>
+            <Routes>
+              <Route path="/" element={<Navigate to="/home" replace />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/myevents" element={<MyEvents />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/profilecard" element={<ProfileCard />} />
+              <Route path="/clubprofilecard" element={<ClubProfileCard />} />
+              <Route path="/login" element={<AuthPage />} />
+              <Route path="/createevent" element={<CreateEventForm />} />
+              <Route path="/clubpage" element={<ClubDirectory />} />
+              <Route path="/campusevents" element={<CampusEvents />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-      <Footer/>
-    </Router>
+        <Footer />
+      </Router>
     </>
   );
 };
 
 export default App;
-
-
-
